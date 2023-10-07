6eaa3d4a8241781b92f73778a884244fd8e4438c
Fmar <frnandu@gmail.com>
2023-10-07 12:39:21 +0200

    description: Dart Nostr Development Kit

--- test/dart_ndk_test.dart
+++ test/dart_ndk_test.dart
@@ -4,9 +4,5 @@ import 'package:dart_ndk/dart_ndk.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
   });
 }
