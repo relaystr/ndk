2050523979735a1f67584e4019e307fff5741de4
Fmar <_@fmar.link>
2023-10-09 14:06:47 +0200

    cleanup

--- lib/nips/Nip65.dart
+++ lib/nips/Nip65.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 import '../event.dart';
 import '../pubkey_mapping.dart';
 
@@ -7,25 +9,23 @@ class Nip65 {
   Map<String, ReadWriteMarker?> relays = {};
 
   Nip65.fromEvent(Event event) {
-    if (event.tags!=null) {
-      for (var tag in event.tags) {
-        if (tag is List<dynamic>) {
-          var length = tag.length;
-          if (length > 1) {
-            var name = tag[0];
-            var url = tag[1];
-            if (name == "r") {
-              ReadWriteMarker? marker;
-              if (length > 2) {
-                var operType = tag[2];
-                if (operType == "read") {
-                  marker = ReadWriteMarker(read: true);
-                } else if (operType == "write") {
-                  marker = ReadWriteMarker(write: true);
-                }
+    for (var tag in event.tags) {
+      if (tag is List<dynamic>) {
+        var length = tag.length;
+        if (length > 1) {
+          var name = tag[0];
+          var url = tag[1];
+          if (name == "r") {
+            ReadWriteMarker? marker;
+            if (length > 2) {
+              var operType = tag[2];
+              if (operType == "read") {
+                marker = ReadWriteMarker(read: true);
+              } else if (operType == "write") {
+                marker = ReadWriteMarker(write: true);
               }
-              relays[url] = marker;
             }
+            relays[url] = marker;
           }
         }
       }
