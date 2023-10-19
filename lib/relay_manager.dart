9292a4067c308208a6b918a2d551156c9625f431
Fmar <_@fmar.link>
2023-10-19 17:51:12 +0200

    FAIL_RELAY_CONNECT_TRY_AFTER_SECONDS = 60;

--- lib/relay_manager.dart
+++ lib/relay_manager.dart
@@ -27,7 +27,7 @@ class RelayManager {
   static const int DEFAULT_STREAM_IDLE_TIMEOUT = 5;
   static const int DEFAULT_BEST_RELAYS_MIN_COUNT = 2;
   static const int MAX_AUTHORS_PER_REQUEST = 100;
-  static const int FAIL_RELAY_CONNECT_TRY_AFTER_SECONDS = 60 * 5;
+  static const int FAIL_RELAY_CONNECT_TRY_AFTER_SECONDS = 60;
   static const int WEB_SOCKET_PING_INTERVAL_SECONDS = 3;
 
 
