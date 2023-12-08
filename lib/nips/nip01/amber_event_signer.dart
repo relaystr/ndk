b0a60080e7274a6a7291bf6bc79a743ce527a3e0
greenart7c3 <greenart7c3@proton.me>
2023-12-08 10:59:17 -0300

    convert to hex pubkey to npub

--- lib/nips/nip01/amber_event_signer.dart
+++ lib/nips/nip01/amber_event_signer.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:amberflutter/amberflutter.dart';
 import 'package:dart_ndk/nips/nip01/event.dart';
 
+import '../nip19/nip19.dart';
 import 'event_signer.dart';
 
 class AmberEventSigner implements EventSigner {
@@ -14,7 +15,8 @@ class AmberEventSigner implements EventSigner {
 
   @override
   Future<void> sign(Nip01Event event) async {
-    Map<dynamic, dynamic> map = await amber.signEvent(npub: publicKey, event: jsonEncode(event.toJson()));
+    final npub = publicKey.startsWith('npub') ? publicKey : Nip19.encodePubKey(publicKey);
+    Map<dynamic, dynamic> map = await amber.signEvent(npub: npub, event: jsonEncode(event.toJson()));
     if (map!=null) {
       event.sig = map['signature'];
     }
@@ -27,13 +29,15 @@ class AmberEventSigner implements EventSigner {
 
   @override
   Future<String?> decrypt(String msg, String destPubKey) async {
-    Map<dynamic, dynamic> map = await amber.nip04Decrypt( ciphertext: msg, npub: publicKey, pubkey: destPubKey);
+    final npub = publicKey.startsWith('npub') ? publicKey : Nip19.encodePubKey(publicKey);
+    Map<dynamic, dynamic> map = await amber.nip04Decrypt( ciphertext: msg, npub: npub, pubkey: destPubKey);
     return map['signature'];
   }
 
   @override
   Future<String?> encrypt(String msg, String destPubKey) async {
-    Map<dynamic, dynamic> map = await amber.nip04Encrypt( plaintext: msg, npub: publicKey, pubkey: destPubKey);
+    final npub = publicKey.startsWith('npub') ? publicKey : Nip19.encodePubKey(publicKey);
+    Map<dynamic, dynamic> map = await amber.nip04Encrypt( plaintext: msg, npub: npub, pubkey: destPubKey);
     return map['signature'];
   }
 
