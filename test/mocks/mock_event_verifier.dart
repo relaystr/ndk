0189c6ace1cc1c327a95b5bda3dd31bf5551cab9
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-03-15 16:08:22 +0100

    more dynamic MockEventVerifier

--- test/mocks/mock_event_verifier.dart
+++ test/mocks/mock_event_verifier.dart
@@ -2,8 +2,15 @@ import 'package:dart_ndk/nips/nip01/event.dart';
 import 'package:dart_ndk/nips/nip01/event_verifier.dart';
 
 class MockEventVerifier implements EventVerifier {
+  bool _result = true;
+
+  /// If [result] is false, [verify] will always return false. Default is true.
+  MockEventVerifier({bool result = true}) {
+    _result = result;
+  }
+
   @override
   Future<bool> verify(Nip01Event event) async {
-    return true;
+    return _result;
   }
 }
