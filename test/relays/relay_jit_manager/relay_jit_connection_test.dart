2960a0124135a6e5636199bae855b7cf7a396137
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-03-15 15:35:27 +0100

    fix mock relay lifecycle

---
+++ test/relays/relay_jit_manager/relay_jit_connection_test.dart
@@ -0,0 +1,52 @@
+import 'package:dart_ndk/nips/nip01/bip340.dart';
+import 'package:dart_ndk/nips/nip01/event.dart';
+import 'package:dart_ndk/nips/nip01/key_pair.dart';
+import 'package:dart_ndk/relay_jit_manager/relay_jit.dart';
+import 'package:dart_ndk/relay_jit_manager/relay_jit_manager.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../mocks/mock_relay.dart';
+
+void main() async {
+  KeyPair key1 = Bip340.generatePrivateKey();
+  KeyPair key2 = Bip340.generatePrivateKey();
+  KeyPair key3 = Bip340.generatePrivateKey();
+  KeyPair key4 = Bip340.generatePrivateKey();
+
+  Map<KeyPair, String> keyNames = {
+    key1: "key1",
+    key2: "key2",
+    key3: "key3",
+    key4: "key4",
+  };
+
+  Nip01Event textNote(KeyPair key2) {
+    return Nip01Event(
+        kind: Nip01Event.TEXT_NODE_KIND,
+        pubKey: key2.publicKey,
+        content: "some note from key ${keyNames[key2]}",
+        tags: [],
+        createdAt: DateTime.now().millisecondsSinceEpoch ~/ 1000);
+  }
+
+  Map<KeyPair, Nip01Event> key1TextNotes = {key1: textNote(key1)};
+  Map<KeyPair, Nip01Event> key2TextNotes = {key2: textNote(key2)};
+  Map<KeyPair, Nip01Event> key3TextNotes = {key3: textNote(key3)};
+  Map<KeyPair, Nip01Event> key4TextNotes = {key4: textNote(key4)};
+
+  group('connect to relay', () {
+    test('Connect to relay', () async {
+      MockRelay relay1 = MockRelay(name: "relay 1");
+      await relay1.startServer();
+
+      RelayJit relayJit = RelayJit(relay1.url);
+      var result =
+          await relayJit.connect(connectionSource: ConnectionSource.UNKNOWN);
+
+      expect(result, true);
+
+      //await Future.delayed(Duration(seconds: 5));
+      await relay1.stopServer();
+    });
+  });
+}
