e4594bae3921b59f52ebf5714ca12e8d0f3aa570
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-03-16 12:50:33 +0100

    fix test run in parallel

--- test/relays/relay_jit_manager/relay_jit_connection_test.dart
+++ test/relays/relay_jit_manager/relay_jit_connection_test.dart
@@ -44,9 +44,15 @@ void main() async {
   Map<KeyPair, Nip01Event> key3TextNotes = {key3: textNote(key3)};
   Map<KeyPair, Nip01Event> key4TextNotes = {key4: textNote(key4)};
 
+  MockRelay relay1 = MockRelay(name: "relay 1");
+  MockRelay relay2 = MockRelay(name: "relay 2");
+
+  MockRelay relay21 = MockRelay(name: "relay 1");
+  MockRelay relay22 = MockRelay(name: "relay 2");
+  MockRelay relay23 = MockRelay(name: "relay 3");
+  MockRelay relay24 = MockRelay(name: "relay 4");
+
   group('connection tests', () {
-    MockRelay relay1 = MockRelay(name: "relay 1");
-    MockRelay relay2 = MockRelay(name: "relay 2");
     test('Connect to relay', () async {
       await relay1.startServer();
 
@@ -70,31 +76,26 @@ void main() async {
   });
 
   group("Calculate best relays (internal MOCKs)", () {
-    MockRelay relay1 = MockRelay(name: "relay 1");
-    MockRelay relay2 = MockRelay(name: "relay 2");
-    MockRelay relay3 = MockRelay(name: "relay 3");
-    MockRelay relay4 = MockRelay(name: "relay 4");
-
     Map<String, String> relayNames = {
-      relay1.url: relay1.name,
-      relay2.url: relay2.name,
-      relay3.url: relay3.name,
-      relay4.url: relay4.name,
+      relay21.url: relay21.name,
+      relay22.url: relay22.name,
+      relay23.url: relay23.name,
+      relay24.url: relay24.name,
     };
     Nip65 nip65ForKey1 = Nip65.fromMap(key1.publicKey, {
-      relay1.url: ReadWriteMarker.readWrite,
-      relay2.url: ReadWriteMarker.readWrite,
-      relay3.url: ReadWriteMarker.readWrite,
-      relay4.url: ReadWriteMarker.readWrite,
+      relay21.url: ReadWriteMarker.readWrite,
+      relay22.url: ReadWriteMarker.readWrite,
+      relay23.url: ReadWriteMarker.readWrite,
+      relay24.url: ReadWriteMarker.readWrite,
     });
     Nip65 nip65ForKey2 = Nip65.fromMap(key2.publicKey, {
-      relay1.url: ReadWriteMarker.readWrite,
-      relay2.url: ReadWriteMarker.readWrite,
+      relay21.url: ReadWriteMarker.readWrite,
+      relay22.url: ReadWriteMarker.readWrite,
     });
     Nip65 nip65ForKey3 =
-        Nip65.fromMap(key3.publicKey, {relay1.url: ReadWriteMarker.readWrite});
+        Nip65.fromMap(key3.publicKey, {relay21.url: ReadWriteMarker.readWrite});
     Nip65 nip65ForKey4 =
-        Nip65.fromMap(key4.publicKey, {relay4.url: ReadWriteMarker.readWrite});
+        Nip65.fromMap(key4.publicKey, {relay24.url: ReadWriteMarker.readWrite});
 
     Map<KeyPair, Nip65> nip65s = {
       key1: nip65ForKey1,
@@ -109,29 +110,29 @@ void main() async {
       // r3 -> k1
       // r4 -> k1,k4
       await Future.wait([
-        relay1.startServer(
+        relay21.startServer(
             nip65s: nip65s,
             textNotes: {}
               ..addAll(key1TextNotes)
               ..addAll(key2TextNotes)
               ..addAll(key3TextNotes)),
-        relay2.startServer(
+        relay22.startServer(
             nip65s: nip65s,
             textNotes: {}
               ..addAll(key1TextNotes)
               ..addAll(key2TextNotes)),
-        relay3.startServer(
+        relay23.startServer(
             nip65s: nip65s, textNotes: {}..addAll(key1TextNotes)),
-        relay4.startServer(textNotes: key4TextNotes..addAll(key1TextNotes))
+        relay24.startServer(textNotes: key4TextNotes..addAll(key1TextNotes))
       ]);
     }
 
     stopServers() async {
       await Future.wait([
-        relay1.stopServer(),
-        relay2.stopServer(),
-        relay3.stopServer(),
-        relay4.stopServer(),
+        relay21.stopServer(),
+        relay22.stopServer(),
+        relay23.stopServer(),
+        relay24.stopServer(),
       ]);
     }
 
@@ -140,7 +141,7 @@ void main() async {
 
       CacheManager cacheManager = MemCacheManager();
       RelayJitManager manager = RelayJitManager(
-        seedRelays: [relay1.url, relay2.url, relay3.url, relay4.url],
+        seedRelays: [relay21.url, relay22.url, relay23.url, relay24.url],
         cacheManager: cacheManager,
       );
 
