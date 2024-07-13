3fe1330942f9d234f12691d5d0cfd78d4cabcb2a
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-07-13 12:09:12 +0200

    exports

--- lib/dart_ndk.dart
+++ lib/dart_ndk.dart
@@ -1,5 +1,40 @@
 library dart_ndk;
 
-//! todo: expose via presentation layer
+/**
+ * presentation layer
+ * 
+ */
+
+// todo:
+
+/**
+ * common entities
+ * 
+ */
+export 'domain_layer/entities/filter.dart';
+
+/**
+ *  export classes that need to be injected
+ * 
+ */
+
+/// signers / verifiers
+export 'domain_layer/repositories/event_verifier_repository.dart';
+export 'domain_layer/repositories/event_signer_repository.dart';
+export 'data_layer/repositories/verifiers/acinq_event_verifier.dart';
+export 'data_layer/repositories/verifiers/bip340_event_verifier.dart';
+export 'data_layer/repositories/signers/amber_event_signer.dart';
+export 'data_layer/repositories/signers/bip340_event_signer.dart';
+
+// cache
+export 'domain_layer/repositories/cache_manager.dart';
+export 'data_layer/repositories/cache_manager/mem_cache_manager.dart';
+export 'data_layer/repositories/cache_manager/db_cache_manager.dart';
+
+/**
+ * common usecases
+ * 
+ */
+
 export "domain_layer/usecases/relay_manager.dart";
 export "domain_layer/usecases/relay_jit_manager.dart";
