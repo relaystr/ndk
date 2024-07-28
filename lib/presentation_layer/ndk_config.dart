f3136fd17460238146a3deeed9461b64e28b17f6
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-07-28 14:28:30 +0200

    add  ignore and bootstrap relays to config

--- lib/presentation_layer/ndk_config.dart
+++ lib/presentation_layer/ndk_config.dart
@@ -1,19 +1,32 @@
-import 'package:dart_ndk/dart_ndk.dart';
-
+import '../config/bootstrap_relays.dart';
 import '../domain_layer/repositories/cache_manager.dart';
+import '../domain_layer/repositories/event_signer.dart';
 import '../domain_layer/repositories/event_verifier.dart';
 
 class NdkConfig {
   EventVerifier eventVerifier;
+
   EventSigner eventSigner;
+
+  /// db to cache nostr data
   CacheManager cache;
+
+  /// nostr network engine to use (choose inbox/outbox mode)
   NdkEngine engine;
+
+  /// relays are not considers for inbox/outbox
+  List<String> ignoreRelays;
+
+  /// initial relays to use and discover for inbox/outbox
+  List<String> bootstrapRelays;
 
   NdkConfig({
     required this.eventVerifier,
     required this.eventSigner,
     required this.cache,
     this.engine = NdkEngine.LISTS,
+    this.ignoreRelays = const [],
+    this.bootstrapRelays = DEFAULT_BOOTSTRAP_RELAYS,
   });
 }
 
