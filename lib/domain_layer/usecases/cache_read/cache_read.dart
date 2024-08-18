d09720b3dd1d72c6b5349867d2f18a8031fd11c7
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-08-18 17:58:37 +0200

    cache read impl

--- lib/domain_layer/usecases/cache_read/cache_read.dart
+++ lib/domain_layer/usecases/cache_read/cache_read.dart
@@ -1,4 +1,4 @@
-import '../../../presentation_layer/ndk_request.dart';
+import '../../entities/nip_01_event.dart';
 import '../../entities/request_state.dart';
 import '../../repositories/cache_manager.dart';
 
@@ -10,8 +10,29 @@ class CacheRead {
   /// find matching events in cache return them and remove/update unresolved filters
   resolveUnresolvedFilters({
     required RequestState requestState,
-  }) {
+  }) async {
     final unresolved = requestState.unresolvedFilters;
-    for (var filter in unresolved) {}
+    for (final filter in unresolved) {
+      final List<Nip01Event> foundEvents = [];
+
+      // authors
+      if (filter.authors != null) {
+        final foundAuthors = cacheManager.loadEvents(
+          pubKeys: filter.authors!,
+          kinds: filter.kinds ?? [],
+        );
+        foundEvents.addAll(foundAuthors);
+
+        // remove found authors from unresolved filter
+        filter.authors!.removeWhere(
+          (author) => foundEvents.any((event) => event.pubKey == author),
+        );
+      }
+
+      // write found events to response stream
+      for (final event in foundEvents) {
+        requestState.networkController.add(event);
+      }
+    }
   }
 }
