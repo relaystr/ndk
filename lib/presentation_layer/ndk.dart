ce2cecb880585621792757e7dd7f6334b7907d90
Fmar <frnandu@gmail.com>
2024-08-19 16:35:47 +0200

    add calculateRelaySet, getContactList, getSingleMetadata methods rename ndkConfig to simply config

--- lib/presentation_layer/ndk.dart
+++ lib/presentation_layer/ndk.dart
@@ -1,6 +1,11 @@
+import '../domain_layer/entities/contact_list.dart';
 import '../domain_layer/entities/filter.dart';
+import '../domain_layer/entities/metadata.dart';
+import '../domain_layer/entities/read_write.dart';
+import '../domain_layer/entities/relay_set.dart';
 import '../domain_layer/repositories/event_signer.dart';
 import '../domain_layer/repositories/event_verifier.dart';
+import '../domain_layer/usecases/relay_manager.dart';
 import '../shared/logger/logger.dart';
 import '../shared/nips/nip01/helpers.dart';
 import 'concurrency_check.dart';
@@ -14,15 +19,15 @@ import '../domain_layer/entities/request_state.dart';
 // some global obj that schuld be kept in memory by lib user
 class Ndk {
   // placeholder
-  final NdkConfig ndkConfig;
+  final NdkConfig config;
   static final GlobalState globalState = GlobalState();
 
   // global initialization use to access rdy repositories
   final Initialization _initialization;
 
-  Ndk(this.ndkConfig)
+  Ndk(this.config)
       : _initialization = Initialization(
-          ndkConfig: ndkConfig,
+          ndkConfig: config,
           globalState: globalState,
         );
 
@@ -57,7 +62,7 @@ class Ndk {
 
     /// handle request)
 
-    switch (ndkConfig.engine) {
+    switch (config.engine) {
       case NdkEngine.LISTS:
         //todo: discuss/implement use of unresolvedFilters
         _initialization.relayManager!.handleRequest(state);
@@ -91,6 +96,82 @@ class Ndk {
     return response;
   }
 
+  Future<RelaySet> calculateRelaySet({required String name,
+    required String ownerPubKey,
+    required List<String> pubKeys,
+    required RelayDirection direction,
+    required int relayMinCountPerPubKey,
+    Function(String, int, int)? onProgress}) async {
+    if (_initialization.relayManager==null) {
+      throw UnimplementedError("this engine doesn't support calculation of relay sets");
+    }
+    return await _initialization.relayManager!.calculateRelaySet(name: name, ownerPubKey: ownerPubKey, pubKeys: pubKeys, direction: direction, relayMinCountPerPubKey: relayMinCountPerPubKey);
+  }
+
+  Future<ContactList?> getContactList(String pubKey,
+      {bool forceRefresh = false,
+      int idleTimeout = RelayManager.DEFAULT_STREAM_IDLE_TIMEOUT}) async {
+    ContactList? contactList = config.cache.loadContactList(pubKey);
+    if (contactList == null || forceRefresh) {
+      ContactList? loadedContactList;
+      try {
+
+        await for (final event in query(filters: [Filter(kinds: [ContactList.KIND], authors: [pubKey], limit: 1)])
+            .stream) {
+          if (loadedContactList == null ||
+              loadedContactList.createdAt < event.createdAt) {
+            loadedContactList = ContactList.fromEvent(event);
+          }
+        }
+      } catch (e) {
+        print(e);
+        // probably timeout;
+      }
+      if (loadedContactList != null &&
+          (contactList == null ||
+              contactList.createdAt < loadedContactList.createdAt)) {
+        loadedContactList.loadedTimestamp =
+            DateTime.now().millisecondsSinceEpoch ~/ 1000;
+        await config.cache.saveContactList(loadedContactList);
+        contactList = loadedContactList;
+      }
+    }
+    return contactList;
+  }
+
+  Future<Metadata?> getSingleMetadata(String pubKey,
+      {bool forceRefresh = false,
+      int idleTimeout = RelayManager.DEFAULT_STREAM_IDLE_TIMEOUT}) async {
+    Metadata? metadata = config.cache.loadMetadata(pubKey);
+    if (metadata == null || forceRefresh) {
+      Metadata? loadedMetadata;
+      try {
+        await for (final event in query(filters: [Filter(kinds: [Metadata.KIND], authors: [pubKey], limit: 1)])
+            .stream) {
+          if (loadedMetadata == null ||
+              loadedMetadata.updatedAt == null ||
+              loadedMetadata.updatedAt! < event.createdAt) {
+            loadedMetadata = Metadata.fromEvent(event);
+          }
+        }
+      } catch (e) {
+        // probably timeout;
+      }
+      if (loadedMetadata != null &&
+          (metadata == null ||
+              loadedMetadata.updatedAt == null ||
+              metadata.updatedAt == null ||
+              loadedMetadata.updatedAt! < metadata.updatedAt! ||
+              forceRefresh)) {
+        loadedMetadata.refreshedTimestamp = Helpers.now;
+        await config.cache.saveMetadata(loadedMetadata);
+        metadata = loadedMetadata;
+      }
+    }
+    return metadata;
+  }
+
+
   /// ! this is just an example
   /// event is event to publish
   /// broadcast config (could be optional) defines relays to broadcast to
@@ -101,11 +182,11 @@ class Ndk {
 
   /// hot swap EventVerifier
   changeEventVerifier(EventVerifier newEventVerifier) {
-    ndkConfig.eventVerifier = newEventVerifier;
+    config.eventVerifier = newEventVerifier;
   }
 
   /// hot swap EventSigner
   changeEventSigner(EventSigner newEventSigner) {
-    ndkConfig.eventSigner = newEventSigner;
+    config.eventSigner = newEventSigner;
   }
 }
