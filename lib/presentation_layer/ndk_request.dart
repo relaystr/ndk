a281db5188b189de2eea4090b0e4c1d1ea30e78f
Fmar <frnandu@gmail.com>
2024-08-19 16:36:12 +0200

    add optional relaySet to query & subscription constructors

--- lib/presentation_layer/ndk_request.dart
+++ lib/presentation_layer/ndk_request.dart
@@ -26,6 +26,7 @@ class NdkRequest {
     required this.filters,
     this.desiredCoverage = 2,
     this.closeOnEOSE = true,
+    this.relaySet,
     this.cacheRead = true,
     this.cacheWrite = true,
     this.timeout = RequestDefaults.DEFAULT_STREAM_IDLE_TIMEOUT + 1,
@@ -37,6 +38,7 @@ class NdkRequest {
     required this.filters,
     this.desiredCoverage = 2,
     this.closeOnEOSE = false,
+    this.relaySet,
     this.cacheRead = true,
     this.cacheWrite = true,
   });
