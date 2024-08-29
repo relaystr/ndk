ccadad73520b4b2b99b2afb8ce991551e98ffe93
Fmar <frnandu@gmail.com>
2024-08-29 18:59:07 +0200

    add doVerification to Bip340EventVerifier for faster debug config option

--- lib/data_layer/repositories/verifiers/bip340_event_verifier.dart
+++ lib/data_layer/repositories/verifiers/bip340_event_verifier.dart
@@ -3,8 +3,12 @@ import 'package:ndk/domain_layer/entities/nip_01_event.dart';
 import 'package:ndk/domain_layer/repositories/event_verifier.dart';
 
 class Bip340EventVerifier implements EventVerifier {
+  bool doVerification;
+
+  Bip340EventVerifier({this.doVerification=true});
+
   @override
   Future<bool> verify(Nip01Event event) async {
-    return bip340.verify(event.pubKey, event.id, event.sig);
+    return !doVerification || bip340.verify(event.pubKey, event.id, event.sig);
   }
 }
