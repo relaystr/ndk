3f5214ff9ff91b2e9991793e336b0e10b89ed791
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-08-30 18:17:48 +0200

    test skip flag, passing on data

--- test/usecases/cache_write_test.dart
+++ test/usecases/cache_write_test.dart
@@ -78,6 +78,46 @@ void main() async {
       expect(duplicatedEvents.length, equals(1));
     });
 
-    test('check if skip flags works', () async {});
+    test('check if skip flags works', () async {
+      final CacheManager myCacheManager = MemCacheManager();
+      final CacheWrite myUsecase = CacheWrite(myCacheManager);
+
+      final StreamController<Nip01Event> responseController =
+          StreamController<Nip01Event>.broadcast();
+      final StreamController<Nip01Event> writeController =
+          StreamController<Nip01Event>();
+
+      closeStream() async {
+        await Future.delayed(const Duration(seconds: 1));
+        await responseController.close();
+      }
+
+      myUsecase.saveNetworkResponse(
+        writeToCache: false,
+        networkController: writeController,
+        responseController: responseController,
+      );
+
+      for (final event in myEvens) {
+        writeController.add(event);
+      }
+
+      closeStream();
+      await responseController.stream.toList().then((data) {
+        // check if every event of expectedEvents is in data
+        for (final expectedEvent in expectedEvents) {
+          expect(data.contains(expectedEvent), true);
+        }
+        // check if data has no more events than expectedEvents
+        //? here the events just get passed on, even if they are duplicates!
+        expect(data.length, equals(myEvens.length));
+      });
+
+      //check if events got not saved
+      for (final event in expectedEvents) {
+        final loadedEvent = myCacheManager.loadEvent(event.id);
+        expect(loadedEvent, isNull);
+      }
+    });
   });
 }
