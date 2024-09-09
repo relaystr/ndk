e841356f67e4e3b9411288e002e19b5bda97b85a
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-09-09 13:27:19 +0200

    add convenience getter

--- lib/domain_layer/entities/request_response.dart
+++ lib/domain_layer/entities/request_response.dart
@@ -6,6 +6,8 @@ class NdkResponse {
   String requestId;
   final Stream<Nip01Event> stream;
 
-  //! not sure if we can always inject a stream
+  /// waits until request is complete and returns a list
+  Future<List<Nip01Event>> get future => stream.toList();
+
   NdkResponse(this.requestId, this.stream);
 }
