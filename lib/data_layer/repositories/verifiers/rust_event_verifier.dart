3a5af824618ac364b2d350fd95cdebe39bbe0233
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-09-12 20:32:48 +0200

    wait for rust init

--- lib/data_layer/repositories/verifiers/rust_event_verifier.dart
+++ lib/data_layer/repositories/verifiers/rust_event_verifier.dart
@@ -1,10 +1,27 @@
+import 'dart:async';
+
 import '../../../domain_layer/entities/nip_01_event.dart';
 import '../../../domain_layer/repositories/event_verifier.dart';
 import '../../../rust_bridge/api/event_verifier.dart';
+import '../../../rust_bridge/frb_generated.dart';
 
 class RustEventVerifier implements EventVerifier {
+  Completer<bool> isInitialized = Completer<bool>();
+
+  RustEventVerifier() {
+    init();
+  }
+
+  Future<bool> init() async {
+    await RustLib.init();
+    isInitialized.complete(true);
+    return true;
+  }
+
   @override
   Future<bool> verify(Nip01Event event) async {
+    await isInitialized.future;
+
     return verifySchnorrSignature(
       eventIdHex: event.id,
       pubKeyHex: event.pubKey,
