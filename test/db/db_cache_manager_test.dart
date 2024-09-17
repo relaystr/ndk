8c1d233672c0d1543efb4f7639e1797bec2d0425
Fmar <frnandu@gmail.com>
2024-09-17 15:16:38 +0200

    improve test for both kind & pTag filter

--- test/db/db_cache_manager_test.dart
+++ test/db/db_cache_manager_test.dart
@@ -15,8 +15,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:isar/isar.dart' as isar;
 
 void main() async {
-  await isar.Isar.initialize(
-      "./libisar.so"); //initializeIsarCore(download: true);
+  await isar.Isar.initialize("./libisar.so"); //initializeIsarCore(download: true);
 
   test('DbContactList', () async {
     DbCacheManager cacheManager = DbCacheManager();
@@ -24,12 +23,10 @@ void main() async {
     // int now = DateTime.now().millisecondsSinceEpoch ~/ 1000;
     String contact1 = "contact1";
     String pubKey1 = "pubKey1";
-    DbContactList userContacts =
-        DbContactList(pubKey: pubKey1, contacts: [contact1]);
+    DbContactList userContacts = DbContactList(pubKey: pubKey1, contacts: [contact1]);
     await cacheManager.saveContactList(userContacts);
 
-    DbContactList? loadedUserContacts =
-        cacheManager.loadContactList(pubKey1) as DbContactList?;
+    DbContactList? loadedUserContacts = cacheManager.loadContactList(pubKey1) as DbContactList?;
     expect(loadedUserContacts!.pubKey, userContacts.pubKey);
     expect(loadedUserContacts.contacts, userContacts.contacts);
   });
@@ -39,21 +36,16 @@ void main() async {
     await cacheManager.init();
     int now = DateTime.now().millisecondsSinceEpoch ~/ 1000;
     String pubKey1 = "pubKey1";
-    DbUserRelayList userRelayList = DbUserRelayList(
-        pubKey: pubKey1,
-        items: [DbRelayListItem("wss://bla.com", ReadWriteMarker.readWrite)],
-        createdAt: now,
-        refreshedTimestamp: now);
+    DbUserRelayList userRelayList =
+        DbUserRelayList(pubKey: pubKey1, items: [DbRelayListItem("wss://bla.com", ReadWriteMarker.readWrite)], createdAt: now, refreshedTimestamp: now);
     await cacheManager.saveUserRelayList(userRelayList);
 
-    DbUserRelayList? loadedUserRelayList =
-        cacheManager.loadUserRelayList(pubKey1) as DbUserRelayList?;
+    DbUserRelayList? loadedUserRelayList = cacheManager.loadUserRelayList(pubKey1) as DbUserRelayList?;
     expect(loadedUserRelayList!.id, userRelayList.id);
     expect(loadedUserRelayList.items.length, userRelayList.items.length);
     for (var i = 0; i < userRelayList.items.length; i++) {
       expect(loadedUserRelayList.items[i].url, userRelayList.items[i].url);
-      expect(
-          loadedUserRelayList.items[i].marker, userRelayList.items[i].marker);
+      expect(loadedUserRelayList.items[i].marker, userRelayList.items[i].marker);
     }
   });
   test('DbNip05', () async {
@@ -61,11 +53,7 @@ void main() async {
     await cacheManager.init();
     // int now = DateTime.now().millisecondsSinceEpoch ~/ 1000;
     String pubKey1 = "pubKey1";
-    DbNip05 nip05 = DbNip05(
-        pubKey: pubKey1,
-        nip05: "bla@bla.com",
-        updatedAt: Helpers.now,
-        valid: true);
+    DbNip05 nip05 = DbNip05(pubKey: pubKey1, nip05: "bla@bla.com", updatedAt: Helpers.now, valid: true);
     await cacheManager.saveNip05(nip05);
 
     DbNip05? loadedNip05 = cacheManager.loadNip05(pubKey1) as DbNip05?;
@@ -85,25 +73,19 @@ void main() async {
         pubKey: pubKey1,
         relayMinCountPerPubkey: 2,
         items: [
-          DbRelaySetItem("wss://bla.com", [
-            DbPubkeyMapping(
-                pubKey: pubKey2, marker: ReadWriteMarker.readWrite.name)
-          ])
+          DbRelaySetItem("wss://bla.com", [DbPubkeyMapping(pubKey: pubKey2, marker: ReadWriteMarker.readWrite.name)])
         ],
         // notCoveredPubkeys: [],
         direction: RelayDirection.outbox);
     await cacheManager.saveRelaySet(relaySet);
 
-    DbRelaySet? loadedRelaySet = cacheManager.loadRelaySet(
-        relaySet.name, relaySet.pubKey) as DbRelaySet?;
+    DbRelaySet? loadedRelaySet = cacheManager.loadRelaySet(relaySet.name, relaySet.pubKey) as DbRelaySet?;
     expect(loadedRelaySet!.id, relaySet.id);
     expect(loadedRelaySet.items.length, relaySet.items.length);
     for (var i = 0; i < relaySet.items.length; i++) {
       expect(loadedRelaySet.items[i].url, relaySet.items[i].url);
-      expect(loadedRelaySet.items[i].pubKeyMappings.length,
-          relaySet.items[i].pubKeyMappings.length);
-      expect(loadedRelaySet.relayMinCountPerPubkey,
-          relaySet.relayMinCountPerPubkey);
+      expect(loadedRelaySet.items[i].pubKeyMappings.length, relaySet.items[i].pubKeyMappings.length);
+      expect(loadedRelaySet.relayMinCountPerPubkey, relaySet.relayMinCountPerPubkey);
       expect(loadedRelaySet.direction, relaySet.direction);
     }
   });
@@ -157,47 +139,14 @@ void main() async {
         sig: 'signature',
         validSig: true,
         sources: ["wss://relay1.com", "wss://relay2.com"]);
-    DbEvent event12 = DbEvent(
-        pubKey: pubKey1,
-        content: "content 12",
-        kind: 2,
-        tags: [],
-        createdAt: now,
-        sig: '',
-        validSig: null,
-        sources: []);
-    DbEvent event21 = DbEvent(
-        pubKey: pubKey2,
-        content: "content 21",
-        kind: 1,
-        tags: [],
-        createdAt: now,
-        sig: '',
-        validSig: null,
-        sources: []);
-    DbEvent event22 = DbEvent(
-        pubKey: pubKey2,
-        content: "content 22",
-        kind: 2,
-        tags: [],
-        createdAt: now,
-        sig: '',
-        validSig: null,
-        sources: []);
-    DbEvent event33 = DbEvent(
-        pubKey: pubKey3,
-        content: "content 33",
-        kind: 3,
-        tags: [],
-        createdAt: now,
-        sig: '',
-        validSig: null,
-        sources: []);
+    DbEvent event12 = DbEvent(pubKey: pubKey1, content: "content 12", kind: 2, tags: [], createdAt: now, sig: '', validSig: null, sources: []);
+    DbEvent event21 = DbEvent(pubKey: pubKey2, content: "content 21", kind: 1, tags: [], createdAt: now, sig: '', validSig: null, sources: []);
+    DbEvent event22 = DbEvent(pubKey: pubKey2, content: "content 22", kind: 2, tags: [], createdAt: now, sig: '', validSig: null, sources: []);
+    DbEvent event33 = DbEvent(pubKey: pubKey3, content: "content 33", kind: 3, tags: [], createdAt: now, sig: '', validSig: null, sources: []);
     cacheManager.removeAllEventsByPubKey(pubKey1);
     cacheManager.removeAllEventsByPubKey(pubKey2);
     cacheManager.removeAllEventsByPubKey(pubKey3);
-    await cacheManager
-        .saveEvents([event11, event12, event21, event22, event33]);
+    await cacheManager.saveEvents([event11, event12, event21, event22, event33]);
 
     Nip01Event? loadedEvent1 = cacheManager.loadEvent(event11.id) as DbEvent?;
     expect(loadedEvent1!.id, event11.id);
@@ -215,40 +164,34 @@ void main() async {
     expect(loadedEventsKind1.contains(event11), true);
     expect(loadedEventsKind1.contains(event21), true);
 
-    List<Nip01Event>? loadedEventsPubkey2 =
-        cacheManager.loadEvents(pubKeys: [pubKey2]);
+    List<Nip01Event>? loadedEventsPubkey2 = cacheManager.loadEvents(pubKeys: [pubKey2]);
     expect(loadedEventsPubkey2.length, 2);
     expect(loadedEventsPubkey2.contains(event21), true);
     expect(loadedEventsPubkey2.contains(event22), true);
 
-    List<Nip01Event>? loadedEventsPubkey1AndKind1 =
-        cacheManager.loadEvents(pubKeys: [pubKey1], kinds: [1]);
+    List<Nip01Event>? loadedEventsPubkey1AndKind1 = cacheManager.loadEvents(pubKeys: [pubKey1], kinds: [1]);
     expect(loadedEventsPubkey1AndKind1.length, 1);
     expect(loadedEventsPubkey1AndKind1.contains(event11), true);
 
-    List<Nip01Event>? loadedEventsPubkey2AndKind2 =
-        cacheManager.loadEvents(pubKeys: [pubKey2], kinds: [2]);
+    List<Nip01Event>? loadedEventsPubkey2AndKind2 = cacheManager.loadEvents(pubKeys: [pubKey2], kinds: [2]);
     expect(loadedEventsPubkey2AndKind2.length, 1);
     expect(loadedEventsPubkey2AndKind2.contains(event22), true);
 
-    List<Nip01Event>? loadedEventsPubkey1AndPubkey2 =
-        cacheManager.loadEvents(pubKeys: [pubKey1, pubKey2]);
+    List<Nip01Event>? loadedEventsPubkey1AndPubkey2 = cacheManager.loadEvents(pubKeys: [pubKey1, pubKey2]);
     expect(loadedEventsPubkey1AndPubkey2.length, 4);
     expect(loadedEventsPubkey1AndPubkey2.contains(event11), true);
     expect(loadedEventsPubkey1AndPubkey2.contains(event12), true);
     expect(loadedEventsPubkey1AndPubkey2.contains(event21), true);
     expect(loadedEventsPubkey1AndPubkey2.contains(event22), true);
 
-    List<Nip01Event>? loadedEventsKind1AndKind2 =
-        cacheManager.loadEvents(kinds: [1, 2]);
+    List<Nip01Event>? loadedEventsKind1AndKind2 = cacheManager.loadEvents(kinds: [1, 2]);
     expect(loadedEventsKind1AndKind2.length, 4);
     expect(loadedEventsKind1AndKind2.contains(event11), true);
     expect(loadedEventsKind1AndKind2.contains(event12), true);
     expect(loadedEventsKind1AndKind2.contains(event21), true);
     expect(loadedEventsKind1AndKind2.contains(event22), true);
 
-    List<Nip01Event>? loadedEventsKind1AndKind3 =
-        cacheManager.loadEvents(kinds: [1, 3]);
+    List<Nip01Event>? loadedEventsKind1AndKind3 = cacheManager.loadEvents(kinds: [1, 3]);
     expect(loadedEventsKind1AndKind3.length, 3);
     expect(loadedEventsKind1AndKind3.contains(event11), true);
     expect(loadedEventsKind1AndKind3.contains(event21), true);
@@ -277,7 +220,7 @@ void main() async {
     DbEvent event12 = DbEvent(
         pubKey: pubKey2,
         content: "content 12",
-        kind: 2,
+        kind: 1,
         tags: [
           ["dupa"],
           ["e", "1w18t12ts281y81"],
@@ -291,13 +234,17 @@ void main() async {
     cacheManager.removeAllEventsByPubKey(pubKey2);
     await cacheManager.saveEvents([event11, event12]);
 
-    List<Nip01Event>? loadedEventsPubkey1 =
-        cacheManager.loadEvents(pTag: pubKey1);
+    List<Nip01Event>? loadedEventsPubkey1 = cacheManager.loadEvents(pTag: pubKey1);
     expect(loadedEventsPubkey1.length, 1);
     expect(loadedEventsPubkey1.first.pTags, event11.pTags);
-    List<Nip01Event>? loadedEventsPubkey2 =
-        cacheManager.loadEvents(pTag: pubKey2);
+    
+    List<Nip01Event>? loadedEventsPubkey2 = cacheManager.loadEvents(pTag: pubKey2);
     expect(loadedEventsPubkey2.length, 1);
     expect(loadedEventsPubkey2.first.pTags, event12.pTags);
+
+    List<Nip01Event>? loadedEventsPubkey2AndKind1 = cacheManager.loadEvents(kinds: [1], pTag: pubKey1);
+    expect(loadedEventsPubkey2AndKind1.length, 1);
+    expect(loadedEventsPubkey2AndKind1.first.pTags, event11.pTags);
+    expect(loadedEventsPubkey2AndKind1.first.kind, 1);
   });
 }
