f943a9f370dd0e56ea6bd30f3d6a7ab56d51bc80
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-09-22 15:54:05 +0200

    request response

--- lib/domain_layer/entities/request_response.dart
+++ lib/domain_layer/entities/request_response.dart
@@ -2,11 +2,19 @@ import 'dart:async';
 
 import 'nip_01_event.dart';
 
+/// Represents a response from a Nostr Development Kit (NDK) request.
 class NdkResponse {
+  /// The unique identifier for the request that generated this response.
   String requestId;
+
+  /// A stream of [Nip01Event] objects returned by the request.
+  ///
+  /// This stream can be listened to for real-time processing of events
+  /// as they arrive from the nostr request.
   final Stream<Nip01Event> stream;
 
-  /// waits until request is complete and returns a list
+  /// A future that resolves to a list of all [Nip01Event] objects
+  /// once the request is complete (EOSE rcv).
   Future<List<Nip01Event>> get future => stream.toList();
 
   NdkResponse(this.requestId, this.stream);
