f9d1d72d88b1220800a465e3d1add03189b63a7d
LeoLox <58687994+leo-lox@users.noreply.github.com>
2024-10-29 19:32:32 +0100

    use default value for timeout

--- lib/domain_layer/usecases/stream_response_cleaner/stream_response_cleaner.dart
+++ lib/domain_layer/usecases/stream_response_cleaner/stream_response_cleaner.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
+import '../../../config/request_defaults.dart';
 import '../../../shared/logger/logger.dart';
 import '../../entities/nip_01_event.dart';
 
-/// given a stream  with Nip01 events it tracks the id and adds the one to the provided stream controller
+/// given a stream  with Nip01 events it tracks the id and adds the one to the provided stream controller \
 /// tracking of the happens in the tracking list
 class StreamResponseCleaner {
   final Set<String> trackingSet;
@@ -15,23 +16,24 @@ class StreamResponseCleaner {
 
   int? timeout;
 
-  /// [trackingSet] a set of ids that are already returned
-  /// [inputStreams] a list of streams that are be listened to
-  /// [outController] the controller that is used to add the events to
-  /// [timeout] the timeout for the stream, if null no timeout is set
+  /// -  [trackingSet] a set of ids that are already returned \
+  /// - [inputStreams] a list of streams that are be listened to \
+  /// - [outController] the controller that is used to add the events to \
+  /// - [timeout] the timeout for the stream, if null [RequestDefaults.DEFAULT_STREAM_IDLE_TIMEOUT] is used
   StreamResponseCleaner({
     required this.trackingSet,
     required this.inputStreams,
     required this.outController,
     required this.timeout,
   }) {
-    if (timeout != null) {
-      Future.delayed(Duration(seconds: timeout!), () {
-        if (!outController.isClosed) {
-          outController.close();
-        }
-      });
-    }
+    Future.delayed(
+        Duration(
+          seconds: timeout ?? RequestDefaults.DEFAULT_STREAM_IDLE_TIMEOUT,
+        ), () {
+      if (!outController.isClosed) {
+        outController.close();
+      }
+    });
   }
 
   void call() {
