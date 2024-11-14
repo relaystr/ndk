f44f97eaa8bc0f4a8385f7c2865688ca3ad448c4
Fmar <frnandu@gmail.com>
2024-11-14 01:08:07 +0100

    separate into melos packages, rust verifier, amber signer, and ndk without flutter dependency

---
+++ packages/ndk/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/usr/bin/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/fmar/workspace/ndk/packages/ndk"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.1.0994"
+export "FLUTTER_BUILD_NUMBER=0.1.0994"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
