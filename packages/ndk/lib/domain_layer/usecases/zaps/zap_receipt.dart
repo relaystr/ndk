6ae6f4f82213dd9cde69de8b58ebf6fc522bfe87
Fmar <frnandu@gmail.com>
2024-12-18 00:48:26 +0100

    initial lnurl & zap support

---
+++ packages/ndk/lib/domain_layer/usecases/zaps/zap_receipt.dart
@@ -0,0 +1,8 @@
+import 'package:ndk/domain_layer/entities/nip_01_event.dart';
+
+class ZapReceipt extends Nip01Event {
+
+  static const int KIND = 9735;
+
+  ZapReceipt({required super.pubKey, required super.kind, required super.tags, required super.content});
+}
