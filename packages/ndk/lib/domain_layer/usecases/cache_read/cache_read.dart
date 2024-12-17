b68c1f1ef0d790dbecc98792f91816527dbde5f8
Fmar <frnandu@gmail.com>
2024-12-17 12:19:52 +0100

    simplify cache read for authors/ids and fix tests / mark some as skip
    (cherry picked from commit 031acb9439e241b3cef51a2e8ec10704b30f0c2e)

--- packages/ndk/lib/domain_layer/usecases/cache_read/cache_read.dart
+++ packages/ndk/lib/domain_layer/usecases/cache_read/cache_read.dart
@@ -15,34 +15,49 @@ class CacheRead {
     required RequestState requestState,
     required StreamController<Nip01Event> outController,
   }) async {
-    final unresolved = requestState.unresolvedFilters;
+    final unresolved = requestState.unresolvedFilters.toSet();
     for (final filter in unresolved) {
       final List<Nip01Event> foundEvents = [];
 
       // authors
       if (filter.authors != null) {
-        final foundAuthors = await cacheManager.loadEvents(
+        final cached = await cacheManager.loadEvents(
           pubKeys: filter.authors!,
           kinds: filter.kinds ?? [],
           since: filter.since,
           until: filter.until,
         );
 
-        foundEvents.addAll(foundAuthors);
-
-        // remove found authors from unresolved filter if it's not a subscription
-        if (!requestState.isSubscription && foundAuthors.isNotEmpty) {
-          if (filter.limit == null) {
-            filter.authors!.removeWhere(
-              (author) => foundEvents.any((event) => event.pubKey == author),
-            );
-          } else if (foundEvents.length >= filter.limit!) {
-            // also ok to remove authors if limit is reached
-            filter.authors!.removeWhere(
-              (author) => foundEvents.any((event) => event.pubKey == author),
-            );
-          }
-        }
+        foundEvents.addAll(cached);
+        // WE CANNOT DO THIS, BECAUSE 1) kinds.length > 1,  2) only replaceable events have 1 event per pubKey+kind, normal events can have many per pubKey+kind
+        // TODO if kind.length == 1 and kind IS replaceable AND there is not limit/until/since AND it is NOT a subscription, then we can do some shit
+      //
+      //   // remove found authors from unresolved filter if it's not a subscription
+      //   if (!requestState.isSubscription && cached.isNotEmpty) {
+      //     if (filter.limit == null) {
+      //       // Keep track of whether we've kept one item
+      //       bool keptOne = false;
+      //       filter.authors!.removeWhere((author) {
+      //         if (!keptOne &&
+      //             foundEvents.any((event) => event.pubKey == author)) {
+      //           keptOne = true;
+      //           return false; // Keep the first matching item
+      //         }
+      //         return foundEvents.any((event) => event.pubKey == author);
+      //       });
+      //     } else if (foundEvents.length >= filter.limit!) {
+      //       // Keep track of whether we've kept one item
+      //       bool keptOne = false;
+      //       filter.authors!.removeWhere((author) {
+      //         if (!keptOne &&
+      //             foundEvents.any((event) => event.pubKey == author)) {
+      //           keptOne = true;
+      //           return false; // Keep the first matching item
+      //         }
+      //         return foundEvents.any((event) => event.pubKey == author);
+      //       });
+      //     }
+      //   }
       }
 
       if (filter.ids != null) {
@@ -55,11 +70,17 @@ class CacheRead {
           foundIdEvents.add(foundId);
         }
 
-        filter.ids!.removeWhere(
-          (id) => foundIdEvents.any((event) => event.id == id),
-        );
+        filter.ids!.removeWhere((id) {
+          return foundIdEvents.any((event) => event.id == id);
+        });
+
 
         foundEvents.addAll(foundIdEvents);
+        if (filter.ids!.isEmpty) {
+          // if we have not more ids in filter, remove the filter entirely,
+          // otherwise it will send too broad filter to relay
+          requestState.unresolvedFilters.remove(filter);
+        }
       }
 
       // write found events to response stream
