d1a5eff252c13b1aa84d9232043cb752b2f9c53f
Fmar <frnandu@gmail.com>
2024-12-21 21:31:38 +0100

    remove unused method

--- packages/ndk/lib/domain_layer/usecases/lnurl/lnurl_response.dart
+++ packages/ndk/lib/domain_layer/usecases/lnurl/lnurl_response.dart
@@ -8,16 +8,6 @@ class LnurlResponse {
   bool? allowsNostr;
   String? nostrPubkey;
 
-  LnurlResponse(
-      {this.callback,
-      this.maxSendable,
-      this.minSendable,
-      this.metadata,
-      this.commentAllowed,
-      this.tag,
-      this.allowsNostr,
-      this.nostrPubkey});
-
   LnurlResponse.fromJson(Map<String, dynamic> json) {
     callback = json['callback'];
     maxSendable = json['maxSendable'];
@@ -27,19 +17,6 @@ class LnurlResponse {
     tag = json['tag'];
     allowsNostr = json['allowsNostr'];
     nostrPubkey = json['nostrPubkey'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['callback'] = callback;
-    data['maxSendable'] = maxSendable;
-    data['minSendable'] = minSendable;
-    data['metadata'] = metadata;
-    data['commentAllowed'] = commentAllowed;
-    data['tag'] = tag;
-    data['allowsNostr'] = allowsNostr;
-    data['nostrPubkey'] = nostrPubkey;
-    return data;
   }
 
   bool get doesAllowsNostr => allowsNostr!=null && allowsNostr!;
