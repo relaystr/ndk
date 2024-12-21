0eabc87996db91c06015f8fb2dd6c4243e3d7684
Fmar <frnandu@gmail.com>
2024-12-21 16:29:52 +0100

    remove kind 2

--- packages/ndk/lib/domain_layer/usecases/zaps/zap_request.dart
+++ packages/ndk/lib/domain_layer/usecases/zaps/zap_request.dart
@@ -4,6 +4,8 @@ class ZapRequest extends Nip01Event {
   static const int KIND = 9734;
 
   ZapRequest(
-      {required super.pubKey, required super.tags, required super.content})
-      : super(kind: KIND);
+      {required String pubKey,
+      required List<List<String>> tags,
+      required String content})
+      : super(kind: KIND, pubKey: pubKey, tags: tags, content: content);
 }
