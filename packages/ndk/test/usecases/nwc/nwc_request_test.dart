4626e7a6e79d5a64866a001aecef9d1b6386d289
Fmar <frnandu@gmail.com>
2024-12-26 01:45:34 +0100

    increase coverage

--- packages/ndk/test/usecases/nwc/nwc_request_test.dart
+++ packages/ndk/test/usecases/nwc/nwc_request_test.dart
@@ -1,12 +1,16 @@
 import 'package:ndk/domain_layer/usecases/nwc/consts/nwc_method.dart';
-import 'package:ndk/domain_layer/usecases/nwc/requests/get_info.dart';
+import 'package:ndk/domain_layer/usecases/nwc/requests/list_transactions.dart';
+import 'package:ndk/domain_layer/usecases/nwc/requests/lookup_invoice.dart';
 import 'package:ndk/domain_layer/usecases/nwc/requests/make_invoice.dart';
+import 'package:ndk/domain_layer/usecases/nwc/requests/multi_pay_invoice.dart';
+import 'package:ndk/domain_layer/usecases/nwc/requests/multi_pay_keysend.dart';
 import 'package:ndk/domain_layer/usecases/nwc/requests/nwc_request.dart';
+import 'package:ndk/domain_layer/usecases/nwc/requests/pay_invoice.dart';
+import 'package:ndk/domain_layer/usecases/nwc/requests/pay_keysend.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('NwcRequest', () {
-
     // KeyPair app = Bip340.generatePrivateKey();
     // KeyPair wallet = Bip340.generatePrivateKey();
     //
@@ -30,48 +34,68 @@ void main() {
     //   expect(request, isA<GetInfoRequest>());
     // });
 
-    test('fromMap should return correct request type for GET_INFO', () {
-      final map = {
-        'method': 'get_info',
-      };
-
+    test('should return ListTransactionsRequest for LIST_TRANSACTIONS', () {
+      final map = {'method': NwcMethod.LIST_TRANSACTIONS.name, 'unpaid': true};
       final request = NwcRequest.fromMap(map);
-
-      expect(request, isA<GetInfoRequest>());
+      expect(request, isA<ListTransactionsRequest>());
     });
 
-    test('fromMap should return correct request type for MAKE_INVOICE', () {
+    test('should return LookupInvoiceRequest for LOOKUP_INVOICE', () {
+      final map = {'method': NwcMethod.LOOKUP_INVOICE.name};
+      final request = NwcRequest.fromMap(map);
+      expect(request, isA<LookupInvoiceRequest>());
+    });
+
+    test('should return MakeInvoiceRequest for MAKE_INVOICE', () {
       final map = {
-        'method': 'make_invoice',
+        'method': NwcMethod.MAKE_INVOICE.name,
         'amount': 1000,
         'description': 'Test invoice',
-        'description_hash': null,
         'expiry': 3600,
       };
-
       final request = NwcRequest.fromMap(map);
-
       expect(request, isA<MakeInvoiceRequest>());
       expect((request as MakeInvoiceRequest).amountSat, 1);
       expect(request.description, 'Test invoice');
     });
 
-    test('fromMap should throw exception for unknown method', () {
-      final map = {
-        'method': 'UNKNOWN_METHOD',
+    test('should return MultiPayInvoiceRequest for MULTI_PAY_INVOICE', () {
+      final map = {'method': NwcMethod.MULTI_PAY_INVOICE.name, 'invoices':[{
+      'invoice': 'i',
+      'amount': 1,
+      }]};
+      final request = NwcRequest.fromMap(map);
+      expect(request, isA<MultiPayInvoiceRequest>());
+    });
+
+    test('should return MultiPayKeysendRequest for MULTI_PAY_KEYSEND', () {
+      final map = {'method': NwcMethod.MULTI_PAY_KEYSEND.name, 'keysends':[{
+        'pubkey': 'p',
+        'amount': 1,
+        'preimage': 'p',
+        'tlv_records':[]
+      }]};
+      final request = NwcRequest.fromMap(map);
+      expect(request, isA<MultiPayKeysendRequest>());
+    });
+
+    test('should return PayInvoiceRequest for PAY_INVOICE', () {
+      final map = {'method': NwcMethod.PAY_INVOICE.name, 'invoice':''};
+      final request = NwcRequest.fromMap(map);
+      expect(request, isA<PayInvoiceRequest>());
+    });
+
+    test('should return PayKeysendRequest for PAY_KEYSEND', () {
+      final map = {'method': NwcMethod.PAY_KEYSEND.name,
+        'pubkey': 'p',
+        'amount': 1,
+        'preimage': 'p',
+        'tlv_records':[]
       };
-
-      expect(() => NwcRequest.fromMap(map), throwsException);
+      final request = NwcRequest.fromMap(map);
+      expect(request, isA<PayKeysendRequest>());
     });
 
-    test('toMap should correctly convert NwcRequest to map', () {
-      final request = NwcRequest(method: NwcMethod.GET_INFO);
-
-      final map = request.toMap();
-
-      expect(map['method'], 'get_info');
-    });
-
-    // Add more tests for other methods and edge cases
+    // Add more tests for any additional methods in NwcMethod
   });
 }
