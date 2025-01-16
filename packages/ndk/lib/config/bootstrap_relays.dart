1d468ee516068483c77a333e6cbe1ab52873d7d4
Fmar <frnandu@gmail.com>
2025-01-16 17:57:23 +0100

    comment nostr.wine from bootstrap

--- packages/ndk/lib/config/bootstrap_relays.dart
+++ packages/ndk/lib/config/bootstrap_relays.dart
@@ -5,7 +5,7 @@ const List<String> DEFAULT_BOOTSTRAP_RELAYS = [
   "wss://purplepag.es",
   "wss://relay.damus.io",
   "wss://nos.lol",
-  "wss://nostr.wine",
+  // "wss://nostr.wine",
   // "wss://offchain.pub",
   // "wss://relay.mostr.pub"
 ];
