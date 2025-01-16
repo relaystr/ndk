88a07f2948832f04a42111756c3360d68aa346e5
Fmar <frnandu@gmail.com>
2025-01-16 17:58:18 +0100

    improve relays display on sample

--- packages/sample-app/lib/main.dart
+++ packages/sample-app/lib/main.dart
@@ -14,6 +14,7 @@ final ndk = Ndk(
   NdkConfig(
     eventVerifier: RustEventVerifier(),
     cache: MemCacheManager(),
+    logLevel: Logger.logLevels.trace
   ),
 );
 
--- packages/sample-app/lib/relays_page.dart
+++ packages/sample-app/lib/relays_page.dart
@@ -1,6 +1,8 @@
 import 'package:amberflutter/amberflutter.dart';
 import 'package:flutter/material.dart';
+import 'package:ndk/domain_layer/entities/read_write_marker.dart';
 import 'package:ndk/domain_layer/entities/user_relay_list.dart';
+import 'package:ndk/shared/nips/nip19/nip19.dart';
 
 import 'main.dart';
 
@@ -15,6 +17,7 @@ class _RelaysPageState extends State<RelaysPage> {
   final amber = Amberflutter();
   // ignore: unused_field
   String _text = '';
+  UserRelayList? relays;
 
   @override
   Widget build(BuildContext context) {
@@ -39,16 +42,22 @@ class _RelaysPageState extends State<RelaysPage> {
                       ),
                     ],
                   ).then((value) async {
-                    UserRelayList? response = await ndk.userRelayLists
-                        .getSingleUserRelayList(value['signature']);
+                    UserRelayList? list = await ndk.userRelayLists
+                        .getSingleUserRelayList(Nip19.decode(value['signature']));
                     setState(() {
-                      _text = response!.relays.toString();
+                      relays = list;
                     });
                   });
                 },
                 child: const Text("Fetch relay list"))
           ],
         )));
+    if (relays!=null) {
+      relays!.relays.keys.forEach((url) {
+        ReadWriteMarker? marker = relays!.relays[url];
+        widgets.add(Row(children: [Text(url), const Text(" "), Text((marker!.isRead?"Read ":" ")+(marker.isWrite?"Write":""))],));
+      });
+    }
     return Column(
         mainAxisAlignment: MainAxisAlignment.start, children: widgets);
   }
