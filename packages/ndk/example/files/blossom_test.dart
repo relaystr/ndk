b0374885f458a674fd740a6ad7021db9b5a31721
LeoLox <58687994+leo-lox@users.noreply.github.com>
2025-01-20 17:43:43 +0100

    files, blossom show tests

---
+++ packages/ndk/example/files/blossom_test.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: avoid_print
+
+import 'package:ndk/ndk.dart';
+import 'package:test/test.dart';
+
+void main() async {
+  test('download test', () async {
+    final ndk = Ndk.defaultConfig();
+
+    final downloadResult = await ndk.blossom.getBlob(
+      sha256:
+          "b1674191a88ec5cdd733e4240a81803105dc412d6c6708d53ab94fc248f4f553",
+      serverUrls: ["https://cdn.hzrd149.com"],
+    );
+
+    print(
+      "file of type: ${downloadResult.mimeType}, size: ${downloadResult.data.length}",
+    );
+
+    expect(downloadResult.data.length, greaterThan(0));
+  });
+}
---
+++ packages/ndk/example/files/files_test.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: avoid_print
+
+import 'package:ndk/ndk.dart';
+import 'package:test/test.dart';
+
+void main() async {
+  test('download test', () async {
+    final ndk = Ndk.defaultConfig();
+
+    final downloadResult = await ndk.files.download(
+        url:
+            "https://cdn.hzrd149.com/b1674191a88ec5cdd733e4240a81803105dc412d6c6708d53ab94fc248f4f553.pdf");
+
+    print(
+        "file of type: ${downloadResult.mimeType}, size: ${downloadResult.data.length}");
+
+    expect(downloadResult.data.length, greaterThan(0));
+  });
+}
