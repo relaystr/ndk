e025c1c11718fbbf29bacccece6a88f94ed281cc
LeoLox <58687994+leo-lox@users.noreply.github.com>
2025-02-14 16:04:32 +0100

    skip real world test

--- packages/ndk/example/account_test.dart
+++ packages/ndk/example/account_test.dart
@@ -7,32 +7,36 @@ import 'package:test/test.dart';
 import 'package:ndk/ndk.dart';
 
 void main() async {
-  test('account', () async {
-    // Create an instance of Ndk
-    // It's recommended to keep this instance global as it holds critical application state
-    final ndk = Ndk.defaultConfig();
+  test(
+    'account',
+    () async {
+      // Create an instance of Ndk
+      // It's recommended to keep this instance global as it holds critical application state
+      final ndk = Ndk.defaultConfig();
 
-    // generate a new key
-    KeyPair key1 = Bip340.generatePrivateKey();
+      // generate a new key
+      KeyPair key1 = Bip340.generatePrivateKey();
 
-    // login using private key
-    ndk.accounts
-        .loginPrivateKey(privkey: key1.privateKey!, pubkey: key1.publicKey);
+      // login using private key
+      ndk.accounts
+          .loginPrivateKey(privkey: key1.privateKey!, pubkey: key1.publicKey);
 
-    // broadcast a new event using the logged in account with it's signer to sign
-    NdkBroadcastResponse response = ndk.broadcast.broadcast(
-        nostrEvent: Nip01Event(
-            pubKey: key1.publicKey,
-            kind: Nip01Event.kTextNodeKind,
-            tags: [],
-            content: "test"),
-        specificRelays: DEFAULT_BOOTSTRAP_RELAYS);
-    await response.broadcastDoneFuture;
+      // broadcast a new event using the logged in account with it's signer to sign
+      NdkBroadcastResponse response = ndk.broadcast.broadcast(
+          nostrEvent: Nip01Event(
+              pubKey: key1.publicKey,
+              kind: Nip01Event.kTextNodeKind,
+              tags: [],
+              content: "test"),
+          specificRelays: DEFAULT_BOOTSTRAP_RELAYS);
+      await response.broadcastDoneFuture;
 
-    // logout
-    ndk.accounts.logout();
+      // logout
+      ndk.accounts.logout();
 
-    // destroy ndk instance
-    ndk.destroy();
-  });
+      // destroy ndk instance
+      ndk.destroy();
+    },
+    skip: true,
+  );
 }
