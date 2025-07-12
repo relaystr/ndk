c21f6e5e86e84d0784799817fe58fe0396953ddc
LeoLox <58687994+leo-lox@users.noreply.github.com>
2025-07-12 11:17:40 +0200

    docs: mention web signer

--- packages/nip07_event_signer/example/nip07_event_signer_example.dart
+++ packages/nip07_event_signer/example/nip07_event_signer_example.dart
@@ -1,5 +1,18 @@
+import 'package:ndk/ndk.dart';
+
+/// import web signer package
 import 'package:nip07_event_signer/nip07_event_signer.dart';
 
 void main() async {
-  await Nip07EventSigner().getPublicKeyAsync();
+  /// create ndk obj
+  final ndk = Ndk.defaultConfig();
+
+  /// create web signer
+  final webSigner = Nip07EventSigner();
+
+  /// on web you need to call this first!
+  await webSigner.getPublicKeyAsync();
+
+  /// login with the web signer
+  ndk.accounts.loginExternalSigner(signer: webSigner);
 }
