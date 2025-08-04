e9fcad61e6af852c16672c2f2b7ce1803e28f2d8
LeoLox <58687994+leo-lox@users.noreply.github.com>
2025-08-04 17:12:35 +0200

    cashu decorator for db transactions

---
+++ packages/ndk/lib/domain_layer/usecases/cashu_wallet/cashu_cache_decorator.dart
@@ -0,0 +1,70 @@
+import 'package:synchronized/synchronized.dart';
+
+import '../../entities/cashu/wallet_cashu_proof.dart';
+import '../../repositories/cache_manager.dart';
+
+class CashuCacheDecorator implements CacheManager {
+  final Lock _operationLock = Lock();
+  final CacheManager _delegate;
+
+  CashuCacheDecorator({
+    required CacheManager cacheManager,
+  }) : _delegate = cacheManager;
+
+  @override
+  Future<void> saveProofs({
+    required List<WalletCashuProof> tokens,
+    required String mintUrl,
+  }) async {
+    await _operationLock.synchronized(() async {
+      await _delegate.saveProofs(tokens: tokens, mintUrl: mintUrl);
+    });
+  }
+
+  @override
+  Future<void> removeProofs({
+    required List<WalletCashuProof> proofs,
+    required String mintUrl,
+  }) async {
+    await _operationLock.synchronized(() async {
+      await _delegate.removeProofs(proofs: proofs, mintUrl: mintUrl);
+    });
+  }
+
+  @override
+  Future<List<WalletCashuProof>> getProofs({
+    required String mintUrl,
+    String? keysetId,
+  }) async {
+    return await _operationLock.synchronized(() async {
+      return await _delegate.getProofs(mintUrl: mintUrl, keysetId: keysetId);
+    });
+  }
+
+  // delegate other methods
+  @override
+  dynamic noSuchMethod(Invocation invocation) {
+    return Function.apply(
+      (_delegate as dynamic)[invocation.memberName],
+      invocation.positionalArguments,
+      invocation.namedArguments,
+    );
+  }
+
+  Future<T> runInTransaction<T>(Future<T> Function() action) async {
+    return await _operationLock.synchronized(() async {
+      return await action();
+    });
+  }
+
+  Future<void> atomicSaveAndRemove({
+    required List<WalletCashuProof> proofsToRemove,
+    required List<WalletCashuProof> tokensToSave,
+    required String mintUrl,
+  }) async {
+    await runInTransaction(() async {
+      await _delegate.removeProofs(proofs: proofsToRemove, mintUrl: mintUrl);
+      await _delegate.saveProofs(tokens: tokensToSave, mintUrl: mintUrl);
+    });
+  }
+}
