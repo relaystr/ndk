c63f96a5f1049e0f5d33feca43e4ee74707b66df
Fmar <frnandu@gmail.com>
2025-08-07 01:17:07 +0200

    add missing export

--- packages/ndk/lib/ndk.dart
+++ packages/ndk/lib/ndk.dart
@@ -81,6 +81,7 @@ export 'domain_layer/usecases/gift_wrap/gift_wrap.dart';
 export 'domain_layer/usecases/bunkers/bunkers.dart';
 export 'domain_layer/usecases/bunkers/models/bunker_connection.dart';
 export 'domain_layer/usecases/bunkers/models/bunker_event.dart';
+export 'domain_layer/usecases/bunkers/models/nostr_connect.dart';
 
 /**
  * other stuff
