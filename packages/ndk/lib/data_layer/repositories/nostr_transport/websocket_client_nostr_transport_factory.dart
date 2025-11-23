914ce825483f47d12c76e6d7ba3094301ec61c33
1leo <58687994+1-leo@users.noreply.github.com>
2025-11-23 12:28:02 +0100

    disable code to expose the bug

--- packages/ndk/lib/data_layer/repositories/nostr_transport/websocket_client_nostr_transport_factory.dart
+++ packages/ndk/lib/data_layer/repositories/nostr_transport/websocket_client_nostr_transport_factory.dart
@@ -1,8 +1,10 @@
 import 'package:ndk/data_layer/repositories/nostr_transport/websocket_client_nostr_transport.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:web_socket_client/web_socket_client.dart';
 
 import '../../../domain_layer/repositories/nostr_transport.dart';
 import '../../../shared/helpers/relay_helper.dart';
+import '../../data_sources/websocket_channel.dart';
 import '../../data_sources/websocket_client.dart';
 
 class WebSocketClientNostrTransportFactory implements NostrTransportFactory {
@@ -19,6 +21,8 @@ class WebSocketClientNostrTransportFactory implements NostrTransportFactory {
     final client = WebSocket(Uri.parse(myUrl), backoff: backoff);
 
     final WebsocketDSClient myDataSource = WebsocketDSClient(client, myUrl);
+    // final WebsocketChannelDS myDataSource =
+    //     WebsocketChannelDS(WebSocketChannel.connect(Uri.parse(myUrl)), myUrl);
     return WebSocketClientNostrTransport(myDataSource, onReconnect);
   }
 }
