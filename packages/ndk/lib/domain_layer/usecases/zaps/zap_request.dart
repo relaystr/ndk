0413531897d5a0a4ef7ec9844f23f5bee3b2a6f4
1leo <58687994+1-leo@users.noreply.github.com>
2025-12-06 10:50:47 +0100

    refactor: zap request calc id

--- packages/ndk/lib/domain_layer/usecases/zaps/zap_request.dart
+++ packages/ndk/lib/domain_layer/usecases/zaps/zap_request.dart
@@ -1,16 +1,40 @@
 import '../../entities/nip_01_event.dart';
+import '../nip_01_event_service/nip_01_event_service.dart';
 
 /// Zap Request
 class ZapRequest extends Nip01Event {
   static const kZapRequestKind = 9734;
 
   /// Zap Request
-  ZapRequest(
-      {required super.pubKey, required super.tags, required super.content})
-      : super(
+  ZapRequest._({
+    required super.pubKey,
+    required super.tags,
+    required super.content,
+    required super.id,
+  }) : super(
           kind: kZapRequestKind,
           sig: null,
-          id: '',
           validSig: null,
         );
+
+  factory ZapRequest({
+    required String pubKey,
+    required List<List<String>> tags,
+    required String content,
+  }) {
+    final calculatedId = Nip01EventService.calculateEventIdSync(
+      pubKey: pubKey,
+      createdAt: DateTime.now().millisecondsSinceEpoch ~/ 1000,
+      kind: kZapRequestKind,
+      tags: tags,
+      content: content,
+    );
+
+    return ZapRequest._(
+      pubKey: pubKey,
+      tags: tags,
+      content: content,
+      id: calculatedId,
+    );
+  }
 }
