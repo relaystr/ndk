753609caa4b2e1917168cd190d8ad75c4ea54788
fmar <frnandu@atomicmail.io>
2025-12-22 01:34:54 +0100

    use isolate.run

--- packages/ndk/lib/data_layer/repositories/verifiers/bip340_event_verifier.dart
+++ packages/ndk/lib/data_layer/repositories/verifiers/bip340_event_verifier.dart
@@ -1,3 +1,5 @@
+import 'dart:isolate';
+
 import 'package:bip340/bip340.dart' as bip340;
 
 import '../../../domain_layer/entities/nip_01_event.dart';
@@ -8,6 +10,8 @@ import '../../../domain_layer/repositories/event_verifier.dart';
 class Bip340EventVerifier implements EventVerifier {
   @override
   Future<bool> verify(Nip01Event event) async {
-    return bip340.verify(event.pubKey, event.id, event.sig);
+    return await Isolate.run(() {
+      return bip340.verify(event.pubKey, event.id, event.sig);
+    });
   }
 }
