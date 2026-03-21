81b03dddc17574571b76ef800e1ecafe2daac1f1
Leo <58687994+1-leo@users.noreply.github.com>
2026-03-21 16:25:37 +0100

    feat: export amberflutter in ndk_amber package

--- packages/amber/lib/ndk_amber.dart
+++ packages/amber/lib/ndk_amber.dart
@@ -8,3 +8,4 @@ library;
 /// signers / verifiers
 export 'data_layer/repositories/signers/amber_event_signer.dart';
 export 'data_layer/data_sources/amber_flutter.dart';
+export 'package:amberflutter/amberflutter.dart';
