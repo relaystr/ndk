841269dc00b45a0e59c18268066a04f7f63bd0a0
Nogringo <npub1kg4sdvz3l4fr99n2jdz2vdxe2mpacva87hkdetv76ywacsfq5leqquw5te@uid.ovh>
2026-06-15 13:53:47 +0200

    test: cover stale replaceable live subscription suppression

--- packages/ndk/test/mocks/mock_relay_live_subscription_test.dart
+++ packages/ndk/test/mocks/mock_relay_live_subscription_test.dart
@@ -87,5 +87,105 @@ void main() {
       expect(eventFromSubscription.id, equals(signedEvent.id));
       expect(eventFromSubscription.content, equals('live subscription event'));
     });
+
+    test('stale replaceable events are not broadcast to subscriptions',
+        () async {
+      final keyPair = Bip340.generatePrivateKey();
+
+      final ndkA = Ndk(
+        NdkConfig(
+          cache: MemCacheManager(),
+          eventVerifier: MockEventVerifier(),
+          bootstrapRelays: [mockRelay.url],
+        ),
+      );
+      addTearDown(ndkA.destroy);
+
+      final ndkB = Ndk(
+        NdkConfig(
+          cache: MemCacheManager(),
+          eventVerifier: MockEventVerifier(),
+          bootstrapRelays: [mockRelay.url],
+        ),
+      );
+      addTearDown(ndkB.destroy);
+
+      final receivedEvents = <Nip01Event>[];
+      final firstEventReceived = Completer<Nip01Event>();
+      final unexpectedSecondEvent = Completer<Nip01Event>();
+      final subscription = ndkA.requests.subscription(
+        filter: Filter(
+          kinds: [10000],
+          authors: [keyPair.publicKey],
+        ),
+      );
+
+      subscription.stream.listen((event) {
+        receivedEvents.add(event);
+        if (!firstEventReceived.isCompleted) {
+          firstEventReceived.complete(event);
+        } else if (!unexpectedSecondEvent.isCompleted) {
+          unexpectedSecondEvent.complete(event);
+        }
+      });
+
+      await Future.delayed(Duration(milliseconds: 200));
+
+      final now = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+      final currentEvent = Nip01Event(
+        pubKey: keyPair.publicKey,
+        kind: 10000,
+        tags: [],
+        content: 'current replaceable event',
+        createdAt: now,
+      );
+      final signedCurrentEvent = Nip01Utils.signWithPrivateKey(
+        event: currentEvent,
+        privateKey: keyPair.privateKey!,
+      );
+
+      await ndkB.broadcast.broadcast(
+        nostrEvent: signedCurrentEvent,
+        specificRelays: [mockRelay.url],
+      ).broadcastDoneFuture;
+
+      final firstEventFromSubscription =
+          await firstEventReceived.future.timeout(
+        Duration(seconds: 2),
+        onTimeout: () => throw TimeoutException(
+          'NDK A did not receive the current replaceable event.',
+        ),
+      );
+
+      final staleEvent = Nip01Event(
+        pubKey: keyPair.publicKey,
+        kind: 10000,
+        tags: [],
+        content: 'stale replaceable event',
+        createdAt: now - 100,
+      );
+      final signedStaleEvent = Nip01Utils.signWithPrivateKey(
+        event: staleEvent,
+        privateKey: keyPair.privateKey!,
+      );
+
+      await ndkB.broadcast.broadcast(
+        nostrEvent: signedStaleEvent,
+        specificRelays: [mockRelay.url],
+      ).broadcastDoneFuture;
+
+      final staleEventWasBroadcast = await unexpectedSecondEvent.future
+          .then((_) => true)
+          .timeout(Duration(milliseconds: 300), onTimeout: () => false);
+
+      expect(firstEventFromSubscription.id, equals(signedCurrentEvent.id));
+      expect(receivedEvents, hasLength(1));
+      expect(receivedEvents.single.id, equals(signedCurrentEvent.id));
+      expect(
+        staleEventWasBroadcast,
+        isFalse,
+        reason: 'Stale replaceable events should not be broadcast live.',
+      );
+    });
   });
 }
